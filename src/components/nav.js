f2c1629ac9bccae5b28fab0f85546b3364a25b5f
Ben Parisot <me@benparisot.com>
2020-07-07 10:31:25 -0700

    styles nav options in nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -18,6 +18,8 @@ const NavMain = styled.nav`
   height: 42px;
   width: 100vw;
   display: flex;
+  justify-content: flex-end;
+  padding-right: 15px;
   background: linear-gradient(
     to right,
     #F04D4D 16.66%, #ED4B64 16.66%,
@@ -26,6 +28,11 @@ const NavMain = styled.nav`
     #F26639 66.66%, #ED4B64 66.66%,
     #ED4B64 83.33%, #F04D4D 83.33%
   );
+    p {
+      color: #F0EA49;
+      margin: auto 20px;
+      font-style: italic;
+    }
 `
 
 export default Nav
