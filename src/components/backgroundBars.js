ba7a9e3cb6c3c2ca9d53802d1a132ff4935d438b
Ben Parisot <me@benparisot.com>
2020-07-06 22:13:27 -0700

    styles first nav option on landing

--- src/components/backgroundBars.js
+++ src/components/backgroundBars.js
@@ -7,7 +7,7 @@ const BackgroundBars = () => {
         style={{
           gridColumnStart: "1",
           gridRowStart: "1",
-          gridRowEnd: "6",
+          gridRowEnd: "span 6",
           backgroundColor: "#F04D4D",
         }}
       ></div>
@@ -15,7 +15,7 @@ const BackgroundBars = () => {
         style={{
           gridColumnStart: "2",
           gridRowStart: "1",
-          gridRowEnd: "6",
+          gridRowEnd: "span 6",
           backgroundColor: "#ED4B64",
         }}
       ></div>
@@ -23,7 +23,7 @@ const BackgroundBars = () => {
         style={{
           gridColumnStart: "3",
           gridRowStart: "1",
-          gridRowEnd: "6",
+          gridRowEnd: "span 6",
           backgroundColor: "#F04D4D",
         }}
       ></div>
@@ -31,7 +31,7 @@ const BackgroundBars = () => {
         style={{
           gridColumnStart: "4",
           gridRowStart: "1",
-          gridRowEnd: "6",
+          gridRowEnd: "span 6",
           backgroundColor: "#F26639",
         }}
       ></div>
@@ -39,7 +39,7 @@ const BackgroundBars = () => {
         style={{
           gridColumnStart: "5",
           gridRowStart: "1",
-          gridRowEnd: "6",
+          gridRowEnd: "span 6",
           backgroundColor: "#ED4B64",
         }}
       ></div>
@@ -47,13 +47,14 @@ const BackgroundBars = () => {
         style={{
           gridColumnStart: "6",
           gridRowStart: "1",
-          gridRowEnd: "6",
+          gridRowEnd: "span 6",
           backgroundColor: "#F04D4D",
         }}
       ></div>
       <Slogan>
         <h1>Ben can help you build that.</h1>
       </Slogan>
+      <Dev><h1>Developer</h1></Dev>
     </Background>
   )
 }
@@ -61,9 +62,9 @@ const BackgroundBars = () => {
 const Background = styled.div`
   display: grid;
   grid-template-columns: 16% 16% 16% 16% 16% auto;
-  grid-template-rows: auto auto auto auto auto auto;
+  grid-template-rows: 16% 16% auto 16% 16% 16%;
   width: 100vw;
-  height: 120vh;
+  height: 100vh;
 `
 
 const Slogan = styled.div`
@@ -74,7 +75,19 @@ const Slogan = styled.div`
   position: relative;
   h1 {
     font-size: 3rem;
+    font-style: italic;
   }
 `
 
+const Dev = styled.div`
+  grid-column-start: 1;
+  grid-row-start: 6;
+  text-transform: uppercase;
+  color: #f0ea49;
+  transform: rotate(180deg);
+  h1 {
+    font-size: 6rem;
+    writing-mode: vertical-rl;
+  }
+`
 export default BackgroundBars
