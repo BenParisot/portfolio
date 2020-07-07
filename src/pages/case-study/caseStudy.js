9ba479d13cf71d65a5c817b9b95538733508cb6e
Ben Parisot <me@benparisot.com>
2020-07-07 13:10:04 -0700

    adds new grid layout to case study container

--- src/pages/case-study/caseStudy.js
+++ src/pages/case-study/caseStudy.js
@@ -1,17 +1,49 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 
-const CaseStudy = (props) => {
+const CaseStudy = props => {
   const { title, description, imgUrl } = props
   return (
-    <>
-      <p>{title}</p>
-      <p>{description}</p>
-      <img src={imgUrl} alt={title} />
-      <p>Learn More</p>
-    </>
+    <CaseStudyContainer>
+      <Title>
+        <h4>{title}</h4>
+      </Title>
+      <Description>
+        <p>{description}</p>
+      </Description>
+      <Image>
+        <img src={imgUrl} alt={title} />
+      </Image>
+      <Link>Learn More</Link>
+    </CaseStudyContainer>
   )
-
 }
 
-export default CaseStudy;
+const CaseStudyContainer = styled.div`
+  display: grid;
+  grid-template-columns: 10% 40% 50%;
+`
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  height: 10vh;
+  background-color: #ed4b64;
+  padding-left: 35px;
+  h4 {
+    text-transform: uppercase;
+    margin: 0;
+  }
+`
+
+const Description = styled.div``
+
+const Image = styled.div`
+  width: 60%;
+  grid-column: 3;
+`
+
+const Link = styled.a``
+
+export default CaseStudy
--- src/pages/landing/developer.js
+++ src/pages/landing/developer.js
@@ -2,22 +2,27 @@ import React from "react"
 import styled from "styled-components"
 import Nav from "../../components/nav"
 import SectionTitle from "../../components/sectionTitle"
-import CaseStudy from '../case-study/caseStudy'
+import CaseStudy from "../case-study/caseStudy"
 
 const Developer = () => {
   const title = `Ben Parisot, Full Stack Dev`
-  const caseStudies = [ {
-    title: 'Weather.',
-    description: 'A serverless web app that takes a user-entered United States zip code and draws an interactive 12-hour temperature forecast line graph using the D3 visualization library.',
-    imgUrl: 'https://www.benparisot.com/static/media/weather.a1c06284.jpg'
-  }]
+  const caseStudies = [
+    {
+      title: "Weather.",
+      description:
+        "A serverless web app that takes a user-entered United States zip code and draws an interactive 12-hour temperature forecast line graph using the D3 visualization library.",
+      imgUrl: "https://www.benparisot.com/static/media/weather.a1c06284.jpg",
+    },
+  ]
 
   const caseStudyList = caseStudies.map(caseStudy => {
-    return <CaseStudy 
-      title={caseStudy.title}
-      description={caseStudy.description}
-      imgUrl={caseStudy.imgUrl}
-    />
+    return (
+      <CaseStudy
+        title={caseStudy.title}
+        description={caseStudy.description}
+        imgUrl={caseStudy.imgUrl}
+      />
+    )
   })
 
   return (
@@ -25,23 +30,35 @@ const Developer = () => {
       <Nav />
       <SectionTitle title={title} />
       <Description>
-      I’m a design-driven full stack engineer. I believe good design leads to clear product requirements and better technical understanding. Let's work together to build something elegant and useful.
+        I’m a design-driven full stack engineer. I believe good design leads to
+        clear product requirements and better technical understanding. Let's
+        work together to build something elegant and useful.
       </Description>
-      {caseStudyList}
+      <CaseStudies>
+        <h3>Case Studies:</h3>
+        {caseStudyList}
+      </CaseStudies>
     </DeveloperMain>
   )
 }
 
 const DeveloperMain = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 100%;
   display: grid;
   grid-template-columns: 16% 16% 16% 16% auto 16%;
   grid-template-rows: 12% 16% 10% 16% 16% 16%;
 `
 
-const Description = styled.div` 
-  grid-column: 3 /span 3;
+const Description = styled.div`
+  grid-column: 3 / span 3;
   grid-row-start: 4;
+`
+const CaseStudies = styled.div`
+  grid-column: 2 / span 4;
+  grid-row-start: 5;
+  h3 {
+    text-transform: uppercase;
+  }
 `
 export default Developer
