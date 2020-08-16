0ef2d8a8007845aab052f753f8d930fb7b7e095b
Ben Parisot <me@benparisot.com>
2020-08-16 15:41:05 -0700

    uses mdxrenderer to populate case study detail with study body

--- src/templates/case-study.js
+++ src/templates/case-study.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
-// import { MDXRenderer } from "gatsby-plugin-mdx"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 import styled from "styled-components"
 import Nav from "../components/nav"
 import SectionTitle from "../components/sectionTitle"
@@ -21,48 +21,8 @@ const CaseStudyDetail = props => {
         <p>Front-end lead, scrummaster</p>
       </CaseStudySidebar>
       <CaseStudyBody>
-        <h3>Nunc gravida nisi felis, in venenatis neque blandit maximus. Duis
-        volutpat, mauris sed blandit rhoncus, nibh massa molestie diam, ac
-        feugiat magna velit a mi. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Vestibulum nec consectetur massa, non porta felis.
-        Curabitur justo velit, varius sed euismod et, tempor vel felis. Sed
-        molestie, ante at mollis rutrum, ante velit fermentum massa, sed dapibus
-        mauris dolor cursus quam. Mauris tempus et mauris id commodo.</h3>
-
-        <p>Suspendisse potenti. Pellentesque tortor risus, dictum sit amet libero
-        vel, scelerisque sollicitudin ligula. In hac habitasse platea dictumst.
-        Vestibulum non laoreet turpis, a pulvinar nunc. Donec aliquam tristique
-        sem, nec pharetra leo pulvinar eu. In lorem velit, viverra nec ipsum ac,
-        congue viverra lectus. Nunc egestas mi leo, a semper libero rutrum id.
-        Aliquam bibendum, dolor sit amet sodales viverra, velit nisl posuere
-        neque, at efficitur neque ante eget lorem. Pellentesque habitant morbi
-        tristique senectus et netus et malesuada fames ac turpis egestas. Nunc
-        gravida nisi felis, in venenatis neque blandit maximus. Duis volutpat,
-        mauris sed blandit rhoncus, nibh massa molestie diam, ac feugiat magna
-        velit a mi. Mauris consectetur ut libero et elementum. Phasellus a nibh
-        in ligula consequat congue. Duis nec blandit ante, non lacinia leo.
-        Donec at magna lacinia, volutpat lectus tincidunt, hendrerit nisi.
-        Praesent vestibulum, elit a dictum iaculis, odio mi congue orci, quis
-        elementum odio lectus at mi. In hac habitasse platea dictumst. Sed
-        interdum mollis lacus vel scelerisque. Donec et vestibulum augue. Ut ac
-        bibendum tellus, vitae aliquam purus. Interdum et malesuada fames ac
-        ante ipsum primis in faucibus. Curabitur eu velit ipsum. Aenean
-        fringilla hendrerit ex, eget luctus magna efficitur sit amet. Quisque
-        hendrerit auctor maximus. Mauris rutrum turpis in tempus finibus. Sed
-        ornare auctor quam id gravida. Suspendisse nunc sapien, vulputate ut
-        facilisis quis, rutrum non metus. Nulla vel imperdiet arcu. Nullam eu
-        finibus ipsum. Vivamus eu erat volutpat, placerat dolor at, volutpat
-        enim. Phasellus nibh felis, rutrum quis sem a, commodo pellentesque
-        turpis. Ut scelerisque tincidunt tristique. Aliquam erat volutpat. Nulla
-        est orci, placerat mollis lacus id, fermentum cursus nisi. Etiam
-        ullamcorper ipsum vel dui porttitor pulvinar. Sed aliquam nibh lectus,
-        quis interdum massa convallis in. Phasellus sed nisi consequat, feugiat
-        sem ut, malesuada purus. Vivamus ullamcorper odio enim, non iaculis
-        lacus blandit non. Ut tempor, ligula sed dignissim ornare, magna neque
-        volutpat nisl, non tempor ex odio at tellus. Etiam a erat a tellus
-        tempus vulputate. Integer tempor iaculis orci id lacinia. Proin vel quam
-        scelerisque, tristique turpis a, maximus lectus. Praesent a magna id
-        ligula sollicitudin consequat.</p>
+        <h3>{study.description}</h3>
+        <MDXRenderer>{study.body}</MDXRenderer>
       </CaseStudyBody>
     </CaseStudyDetailContainer>
   )
