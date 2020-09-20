b5d3a84485edbda4a2082a1861558b5e46b19d62
Ben Parisot <me@benparisot.com>
2020-09-20 10:51:56 -0700

    styles footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { respondTo } from '../styling/respondTo'
 const Footer = () => {
   return (
     <FooterContainer>
-    <p>Designed and built by Ben Parisot in 2020</p>
+    <p>Designed and built by Ben Parisot in 2020.</p>
     <a href="#top">Back to the top</a>
   </FooterContainer>
   )
@@ -13,10 +13,16 @@ const Footer = () => {
 
 const FooterContainer = styled.footer`
   text-align: center;
-  margin: 24px;
+  margin-top: 24px;
   ${respondTo.xs`
     visibility: hidden;
   `}
+  background-color: lightgrey;
+  padding: 60px;
+  a {
+    color: white;
+    text-transform: uppercase;
+  }
 `
 
 export default Footer
